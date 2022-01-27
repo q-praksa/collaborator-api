9cf79076e2065e39cc6c3b67981dc2e649dcede9
Srecko Stojic <stojic.srecko@gmail.com>
2022-01-27 09:17:05 +0100

    [CLB-54] Use accessTokenSecret and refreshToken secret from config instead of .env file directly." (#5)
    * [CLB-52] Add get and post methods for users.
    
    * [CLB-52] Add get by id, patch and delete methods.
    
    * [CLB-52] Fix requested changes. Add params.js file.
    
    * [CLB-49] Create Timesheet model.
    
    * [CLB-54] Use access/refreshTokenSecret from config.

--- src/api/auth/middlewares.js
+++ src/api/auth/middlewares.js
@@ -1,18 +1,19 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const { accessTokenSecret } = require('../../config/index');
 
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
+  const authHeader = req.headers['authorization'];
   if (!authHeader) {
     return res.sendStatus(401);
   }
   // We expect the authorization header value to be in format Bearer <token>
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
     return res.sendStatus(401);
   }
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, accessTokenSecret, (err, user) => {
     if (err) {
       return res.sendStatus(403);
     }
