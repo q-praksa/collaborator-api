f45e788f2bcb6fd1456f7f56213885664237c36e
Bojana Buljic <bojana.buljic@gmail.com>
2022-02-17 15:38:50 +0100

    [CLB-77] Fix generating access token on refresh token [BE]

--- src/models/refreshToken.js
+++ src/models/refreshToken.js
@@ -1,35 +1,37 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class RefreshToken extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class RefreshToken extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  RefreshToken.init(
-    {
-      value: {
-        type: DataTypes.STRING,
-        validate: {
-          notEmpty: true,
+    RefreshToken.init(
+        {
+            value: {
+                type: DataTypes.STRING,
+                validate: {
+                    notEmpty: true,
+                },
+                allowNull: false,
+                primaryKey: true,
+            },
+            expires: {
+                type: DataTypes.DATE,
+                defaultValue: new Date(
+                    new Date().getTime() + 30 * 24 * 60 * 60 * 1000
+                ),
+            },
         },
-        allowNull: false,
-        primaryKey: true,
-      },
-      expires: {
-        type: DataTypes.DATE,
-        defaultValue: new Date(new Date().getTime + 30 * 24 * 60 * 60 * 1000),
-      },
-    },
-    {
-      sequelize,
-      modelName: "refreshToken",
-    }
-  );
-  return RefreshToken;
+        {
+            sequelize,
+            modelName: 'refreshToken',
+        }
+    );
+    return RefreshToken;
 };
